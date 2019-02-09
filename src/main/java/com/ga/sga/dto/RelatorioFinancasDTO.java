c3ddb5964a51c1a6325b47df179877bf890def0e
Gustavo Ames <gustavo.ames87@gmail.com>
2019-02-09 17:56:01 -0200

    Thymeleaf

--- src/main/java/com/ga/sga/dto/RelatorioFinancasDTO.java
+++ src/main/java/com/ga/sga/dto/RelatorioFinancasDTO.java
@@ -1,6 +1,18 @@
 package com.ga.sga.dto;
 
+import com.ga.sga.domain.Despesa;
+import com.ga.sga.domain.Receita;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class RelatorioFinancasDTO {
 
-
+    private Despesa despesa;
+    private Receita receita;
 }
